d8735ae8ecdf144ba9c50a870e2786112ed653f8
srishtiiii19 <srishtisri1935@gmail.com>
2026-04-01 14:10:00 +0530

    Srishti added UC1

---
+++ App/src/TrainConsistManagementApp.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainConsistManagementApp {
+    public static void main(String[] args){
+
+        System.out.println("====================================================");
+        System.out.println("\n=== Train Consist Management App ===\n");
+        System.out.println("====================================================\n");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Train initially successfully...");
+    }
+}
